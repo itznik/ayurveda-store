0628b5c2cc3f8490fdc4e900c9ad8aafbe24bbf9
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2025-11-22 04:40:43 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { 
-    addOrderItems, 
-    getOrderById, 
-    getOrders, 
-    getMyOrders, 
-    updateOrderToDelivered 
+const {
+    addOrderItems,
+    getOrderById,
+    getMyOrders,
+    getOrders,
+    updateOrderToDelivered,
 } = require('../controllers/orderController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
-router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
-router.route('/myorders').get(protect, getMyOrders); // <--- The missing link
+router.route('/')
+    .post(protect, addOrderItems)
+    .get(protect, admin, getOrders);
+
+router.route('/myorders').get(protect, getMyOrders); // This makes your Account Page work
+
 router.route('/:id').get(protect, getOrderById);
-router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
+
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered); // Admin can mark delivered
 
 module.exports = router;
