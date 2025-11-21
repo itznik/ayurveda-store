dcf032aeeb8efb5bdfe72c909ea1a8a38c78d216
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2025-11-22 01:17:17 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    role: { 
+        type: String, 
+        enum: ['user', 'admin', 'super_admin'], // Defines who can edit the site
+        default: 'user'
+    },
+    avatar: {
+        type: String,
+        default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+    }
+}, { timestamps: true }); // timestamps = true tracks created/updated time automatically
+
+// Encrypt password automatically before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to check password during login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
