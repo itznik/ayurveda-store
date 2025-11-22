84d2395403f4d5d29a5517f5339b6b1db8ae3515
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2025-11-22 15:55:28 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, authUser } = require('../controllers/authController');
-const { getUserProfile, updateUserProfile } = require('../controllers/userController'); // Import new
+const { getUserProfile, updateUserProfile } = require('../controllers/userController'); // Import from the NEW file
 const { protect } = require('../middleware/authMiddleware');
 
+// Auth Routes
 router.post('/register', registerUser);
 router.post('/login', authUser);
-// New Profile Routes
+
+// Profile Routes (Protected)
 router.route('/profile')
-    .get(protect, getUserProfile)
-    .put(protect, updateUserProfile);
+    .get(protect, getUserProfile)    // Fetch Profile
+    .put(protect, updateUserProfile); // Update Profile
 
 module.exports = router;
